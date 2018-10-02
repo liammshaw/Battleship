dadfa76648c92728e294e8e2ed117e7ddcc4c1cd
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-10-02 14:14:12 -0600

    named ships

--- BattleShip/src/Game.java
+++ BattleShip/src/Game.java
@@ -31,30 +31,55 @@ public class Game
 				if (level == 1)
 					{
 						easy();
+						nameShips();
 					}
 				else if (level == 2)
 					{
 						medium();
+						nameShips();
 					}
 				else 
 					{
 						hard();
+						nameShips();
 					}
 			}
 		public static void easy()
 			{
-				ships.add(new BattleShip(4, ""));
-				ships.add(new BattleShip(3, ""));
-				ships.add(new BattleShip(2, ""));
+				ships.add(new BattleShip(4, "1"));
+				ships.add(new BattleShip(3, "2"));
+				ships.add(new BattleShip(2, "3"));
 				battlefield = new int [5][5];
-				
 			}
 		public static void medium()
 			{
 				battlefield = new int [7][7];
+				ships.add(new BattleShip(4, "1"));
+				ships.add(new BattleShip(3, "2"));
+				ships.add(new BattleShip(2, "3"));
 			}
 		public static void hard()
 			{
 				battlefield = new int [10][10];
+				ships.add(new BattleShip(4, "1"));
+				ships.add(new BattleShip(3, "2"));
+				ships.add(new BattleShip(2, "3"));
+			}
+		public static void nameShips()
+			{
+				System.out.println("It's time name your ships");
+				System.out.println("What would you like to name your longest ship?(size 4)");
+				String name = userString.nextLine();
+				ships.get(0).setName(name);
+				System.out.println("What would you like to name your medium sized ship?(size 3)");
+				name = userString.nextLine();
+				ships.get(1).setName(name);
+				System.out.println("What would you like to name your shortest ship?(size 2)");
+				name = userString.nextLine();
+				ships.get(2).setName(name);
+			}
+		public static void placeShips()
+			{
+				
 			}
 	}
